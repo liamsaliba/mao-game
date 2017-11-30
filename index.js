6bddaaac22b035aaa95518a44e135a428d20b09e
lsp0 <liamsaliba@hotmail.com>
2017-11-30 17:52:50 +1100

    Initial server configuration

---
+++ index.js
@@ -0,0 +1,41 @@
+// function handler : express
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+// Route handler, sends index.html when root address is hit.
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io')(http);
+
+const PORTNUMBER = 6060;
+
+// HTTP server, listen for activity on port
+http.listen(PORTNUMBER, function(){
+	l("Public server started, listening on port " + PORTNUMBER);
+	init();
+});
+
+// logging library
+function l(string) {
+	console.log(new Date().toLocaleString() + " * " + "[" + l.caller.name + "] " + string)
+}
+function e(string){
+	console.error(new Date().toLocaleString() + " ! " + "[" + e.caller.name + "] " + string)
+}
+
+// server start calls
+function init() {
+	l("Server initialised.")
+}
+
+// console input handler
+var stdin = process.openStdin();
+stdin.addListener("data", function(d) {
+	str = d.toString().trim();
+	l(">>" + str);
+});
+
+io.on('connection', (socket) => {
+	l("Connected to client");
+})
